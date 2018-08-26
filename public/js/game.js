c27ac78310408b69ec64966b64bb661bb084e83b
ben <bennappier@gmail.com>
2018-08-26 12:02:56 -0700

    wip: notes on game state

--- public/js/game.js
+++ public/js/game.js
@@ -83,12 +83,88 @@ function create() {
       this.socket.emit('starCollected');
     }, null, self);
   });
+
+  // rawGame state is a string, representing the state of the world the player has loaded
+  // the string is JSON formatted
+  // `loadedLocation` will be the start of the top left corner of what the player has loaded.
+  // The player will see a smaller subset of what they have loaded; their 'vision' will be smaller
+  // than their `loaded` vision.
+
+  // I think it would be nice to have coordinates be there 'true' coordinate position, as in ignoring subworlds.
+  // The coordinates are key values pretty similar to our redis-store.
+
+  // The objects can be keyed by their type. I think this will allow the client to quickly iterate over objects and be
+  // able to easily instantiate them in the client's state.
+
+  // I think the client should update the server / backend with the state of its player but should only be notified form
+  // the server of the state of the player when the server detects the client's state of the player is wrong / needs to be corrected.
+  // This way, the server is sending gameStates to the client that would 'undo' the client's previous move.
+
+  self.updateGameState = (jsonGameState) => {
+    self.loadedLocation = jsonGameState['loadedLocation'];
+    // delete all coordinates outside of 'loaded vision' determined by fixed grid size and `loadedLocation`
+
+    // merge all objects.
+    // Create new objects from `objects` key.
+    // Update existing objects from `objects` key.
+    // Delete existing objects that are now missing from `objects` key.
+
+    // update all coordinates with new objects
+  };
+
+  self.gameStateUpdate = (rawGameState) => {
+    jsonGameState = JSON.parse(rawGameState);
+
+    // stubbed for now
+    jsonGameState = {
+      loadedLocation: "0,0",
+      coordinates: {
+        "0,1": "coin:33",
+        "0,2": "player:1",
+        "0,3": "rock:-1", // is this -1 for now?
+      },
+      objects: {
+        player: {
+          "1": {
+            hp: "10",
+            alive: "true",
+            coinCount: "22",
+          },
+          "2": {
+            hp: "7",
+            alive: "true"
+          }
+        },
+        coin: {
+          "33": {
+            amount: "11",
+          },
+          "2": {
+            amount: "3"
+          }
+        },
+        rock: "", // is this just an empty string for now?
+      },
+    };
+
+    // self.updateGameState(jsonGameState)
+
+    // this 'upserts' (update or inserts) new objects
+    // self.loadObjects(jsonGameState["objects"])
+
+    // set a variable as the loaded location
+    // self.setLoadedLocation(jsonGameState['loadedLocation'])
+  };
+
+  this.socket.on('stateUpdate', self.gameStateUpdate)
 }
 
 function addPlayer(self, playerInfo) {
-  self.ship = self.physics.add.sprite(playerInfo.x, playerInfo.y, 'ship').setOrigin(0.5, 0.5).setDisplaySize(53, 40);
+  self.ship = self.physics.add.sprite(playerInfo.x, playerInfo.y, 'ship')
+    .setOrigin(0.5, 0.5)
+    .setDisplaySize(53, 40);
 
-  self.ship.move = (time, direction)=> {
+  self.ship.move = (time, direction) => {
     if(direction === 'up'){
       self.ship.body.y -= GRID_DISTANCE;
     } else if(direction === 'left') {
@@ -102,19 +178,19 @@ function addPlayer(self, playerInfo) {
     lastMoveTime = time;
   };
 
-  self.ship.moveUp = (time)=> {
+  self.ship.moveUp = (time) => {
     self.ship.move(time, 'up');
   };
 
-  self.ship.moveLeft = (time)=> {
+  self.ship.moveLeft = (time) => {
     self.ship.move(time, 'left');
   };
 
-  self.ship.moveDown = (time)=> {
+  self.ship.moveDown = (time) => {
     self.ship.move(time, 'down');
   };
 
-  self.ship.moveRight = (time)=> {
+  self.ship.moveRight = (time) => {
     self.ship.move(time, 'right');
   };
 
